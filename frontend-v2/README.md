a7d6db3192d6e0d1eb5a9d18fef31303c4745986
bauerkamil <kamilbauer01@gmail.com>
2025-05-31 11:05:37 +0200

    added docker for frontend

--- frontend-v2/README.md
+++
@@ -1,7 +0,0 @@
-```
-# Step 1: Install the necessary dependencies.
-npm i
-
-# Step 2: Start the development server with auto-reloading and an instant preview.
-npm run dev
-```
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
-import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from '@radix-ui/themes';
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Theme>
-    <App />
-    <ThemePanel />
-    </Theme>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<App />);
